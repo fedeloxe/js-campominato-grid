e004b7db5efe7572222d6e4f5dc092d060b00d4f
fedeloxe <loxeee@gmail.com>
2022-12-06 20:38:25 +0100

    grid done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-// Tramite una funzione creo i quadrati 
-function createGridSquare(){
-    const currentElement= document.createElement('div')
+// Tramite una funzione creo la classe dei quadrati 
+function createSquare(i){
+    let currentElement= document.createElement('div')
 
     currentElement.classList.add('square')
+
+    currentElement.innerText= i;
 
     return currentElement;
 }
 
 let grid = document.getElementById('grid')
 
-for(let i=0; i<100; i++){
-    const currentSquare = createGridSquare()
+//aggiungo il bottone play
+
+//tramite un ciclo creo i quadrati e li inserisco allínterno della classe
+for(let i=1; i<= 100; i++){
+    const currentSquare = createSquare(i)
+
+
+    currentSquare.addEventListener('click', function(){
+        this.classList.add('click');
+        console.log('hai cliccaro il numero '+ this.innerText)
+    });
 
     grid.appendChild(currentSquare);
 }
